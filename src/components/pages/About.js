984842013b3d8014d1cc2b3b89c2281d33fce1ba
Leon Forsythe <lvforsythe95@gmail.com>
2023-01-23 18:07:26 -0500

    created about me page and styles

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,25 @@
 import React from "react";
+import bioImg from "../../assets/IMG_0597.JPG";
+import "../../styles/About.css";
 
-export default function About(){
-   return <h1>About me</h1>
+export default function About() {
+  return (
+    <div className="container">
+      <h1>About Me</h1>
+      <img className="bioImg" src={bioImg} />
+      <p>
+        Hey, my name is Leon Forsythe. I am a full-stack software developer
+        passionate about solving problems and making cool user experiences. I'm
+        currently completing a certificate in Full-Stack Development from the
+        University of Pennsylvania.
+      </p>
+      <span>
+        <i class="fa-brands fa-html5"></i>
+        <i class="fa-brands fa-css3-alt"></i>
+        <i class="fa-brands fa-js"></i>
+        <i class="fa-brands fa-bootstrap"></i>
+        <i class="fa-brands fa-node"></i>
+      </span>
+    </div>
+  );
 }
