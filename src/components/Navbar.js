4d69eee06f7738224f1a0c4f72419fcf548a9783
Leon Forsythe <lvforsythe95@gmail.com>
2023-01-23 16:39:27 -0500

    added styles for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,22 +4,22 @@ export default function Navbar({ handlePageChange, page }) {
   return (
     <nav className="navbar">
       <ul>
-        <li>
+        <li className={page === 'about' ? 'active' : ''}>
           <a href="#about" onClick={() => handlePageChange("about")}>
             About Me
           </a>
         </li>
-        <li>
+        <li className={page === 'work' ? 'active' : ''}>
           <a href="#work" onClick={() => handlePageChange("work")}>
             Portfolio
           </a>
         </li>
-        <li>
+        <li className={page === 'contact' ? 'active' : ''}>
           <a href="#contact" onClick={() => handlePageChange("contact")}>
             Contact
           </a>
         </li>
-        <li>
+        <li className={page === 'about' ? 'resume' : ''}>
           <a href="#resume" onClick={() => handlePageChange("resume")}>
             Resume
           </a>
