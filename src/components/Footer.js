f2a4fad6833d3dea1e2c8a862a638f252a4b2927
Leon Forsythe <lvforsythe95@gmail.com>
2023-01-25 09:26:13 -0500

    added height to portfolio container

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
   return (
     <footer className="navbar">
       <span>
-        <a href="https://github.com/hope428"><i class="fa-brands fa-github"></i></a>
-        <a href="https://www.linkedin.com/in/leon-forsythe-54357b239/"><i class="fa-brands fa-linkedin"></i></a>
+        <a target="_blank" href="https://github.com/hope428"><i class="fa-brands fa-github"></i></a>
+        <a target="_blank" href="https://www.linkedin.com/in/leon-forsythe-54357b239/"><i class="fa-brands fa-linkedin"></i></a>
       </span>
     </footer>
   );
