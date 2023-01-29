45e0ad3c86d90627fe0012ed3e4f9acfdbe46ce0
Leon Forsythe <lvforsythe95@gmail.com>
2023-01-29 14:36:53 -0500

    setup emailjs

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,24 +1,28 @@
 import React, { useState, useRef } from "react";
-import "../../styles/Form.css"
-import emailjs from '@emailjs/browser'
+import "../../styles/Form.css";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
-
-  const form = useRef()
+  const form = useRef();
   const [contactName, setContactName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [sent, setSent] = useState(false);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
-      const result = await emailjs.sendForm('service_9eg3uph',
-      'template_4vvi45b', form.current, 'iXTpGEoKKQ0TnCi1z')
+      const result = await emailjs.sendForm(
+        "service_9eg3uph",
+        "template_4vvi45b",
+        form.current,
+        "iXTpGEoKKQ0TnCi1z"
+      );
       console.log(result.text);
     } catch (error) {
       console.log(error.text);
     }
-    
+    setSent(true)
   };
 
   const handleChange = (e) => {
@@ -35,35 +39,39 @@ export default function Contact() {
 
   return (
     <div className="container column about">
-      <h1>Contact Me</h1>
-      <form ref={form} onSubmit={handleFormSubmit}>
-        <input
-          value={contactName}
-          name="contactName"
-          onChange={handleChange}
-          type="text"
-          placeholder="Name"
-          required
-        />
-        <input
-          value={email}
-          name="email"
-          onChange={handleChange}
-          type="email"
-          placeholder="Email"
-          required
-        />
-        <textarea
-          value={message}
-          name="message"
-          onChange={handleChange}
-          placeholder="Enter message..."
-          required
-        ></textarea>
-        <button type="submit">
-          Submit
-        </button>
-      </form>
+      {sent ? (
+        <h1>Your message has been sent!</h1>
+      ) : (
+        <>
+          <h1>Contact Me</h1>
+          <form ref={form} onSubmit={handleFormSubmit}>
+            <input
+              value={contactName}
+              name="contactName"
+              onChange={handleChange}
+              type="text"
+              placeholder="Name"
+              required
+            />
+            <input
+              value={email}
+              name="email"
+              onChange={handleChange}
+              type="email"
+              placeholder="Email"
+              required
+            />
+            <textarea
+              value={message}
+              name="message"
+              onChange={handleChange}
+              placeholder="Enter message..."
+              required
+            ></textarea>
+            <button type="submit">Submit</button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
