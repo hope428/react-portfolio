5ef5b3000da0ca1e099d72c0ea9966262c52bd0b
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-10 18:02:25 -0500

    added margin to top of header section to fix overlapping with navbar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
-    <main>
       <Portfolio />
-    </main>
   );
 }
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import Navbar from "./Navbar";
 import About from "./pages/About"
 import Work from "./pages/Work"
