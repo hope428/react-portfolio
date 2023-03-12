506def0e76feaa5675f125ebffa850f19674f55e
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-12 11:03:52 -0400

    finished contact section and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
-import Portfolio from "./components/Portfolio";
+import Navbar from "./components/Navbar";
+import About from "./components/pages/About";
+import Work from "./components/pages/Work";
+import Contact from "./components/pages/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Skills from "./components/Skills";
 
 function App() {
   return (
-      <Portfolio />
+    <main>
+      <Navbar />
+      <Header />
+      <Skills />
+      <Work />
+      <About />
+      <Contact />
+      <Footer />
+    </main>
   );
 }
 
