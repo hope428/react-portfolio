defef41768b44488eb082d5b6417153508ed5722
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-12 11:12:58 -0400

    updated footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,9 @@ import "../styles/Navbar.css";
 
 export default function Footer() {
   return (
-    <footer className="navbar">
-      <span>
+    <footer className="footer">
         <a target="_blank" href="https://github.com/hope428"><i class="fa-brands fa-github"></i></a>
         <a target="_blank" href="https://www.linkedin.com/in/leon-forsythe-54357b239/"><i class="fa-brands fa-linkedin"></i></a>
-      </span>
     </footer>
   );
 }
