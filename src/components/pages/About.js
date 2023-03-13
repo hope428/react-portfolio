b9b03e3aa8b01b3120fe1f34c8c89ced0622351e
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-13 18:36:41 -0400

    tweaked mobile view

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,13 +7,15 @@ export default function About() {
     <>
       <h1 className="about-header">Meet the developer</h1>
       <div className="about-section" id="about">
-      <img className="bioImg" src={bioImg} alt="Leon outside smiling"/>
-      <p>
-        Hey, my name is Leon Forsythe. I am a full-stack software developer
-        passionate about solving problems and making cool user experiences. I hold a certificate in Full-Stack Development from the
-        University of Pennsylvania and an Associates of Liberal Sciences from the Community College of Allegheny County.
-      </p>
-    </div>
+        <img className="bioImg" src={bioImg} alt="Leon outside smiling" />
+        <p>
+          Hey, my name is Leon Forsythe. I am a full-stack software developer
+          passionate about solving problems and making cool user experiences. I
+          hold a certificate in Full-Stack Development from the University of
+          Pennsylvania and an Associates of Liberal Sciences from the Community
+          College of Allegheny County.
+        </p>
+      </div>
     </>
   );
 }
