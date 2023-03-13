dae0d933729bb10102b5e2551263dab31a2baea0
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-13 17:06:17 -0400

    added color change to navbar on scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,34 @@
-import React from "react";
-import '../styles/Navbar.css'
+import React, { useEffect, useState } from "react";
+import "../styles/Navbar.css";
 import PDF from "../assets/resumefile.pdf";
 
 export default function Navbar() {
+  const [colorChange, setColorChange] = useState(false);
+
+  const changeNavbar = () => {
+    if (window.scrollY > 100) {
+      setColorChange(true);
+    } else {
+      setColorChange(false);
+    }
+  };
+
+  window.addEventListener('scroll', changeNavbar)
+
+  const navbarStyles = colorChange ? "navbar filled" : "navbar transparent"
+
   return (
-    <nav className="navbar">
+    <nav className={navbarStyles}>
       <h1>Leon Forsythe</h1>
       <ul>
         <li>
-          <a href="#about">
-            About Me
-          </a>
+          <a href="#about">About Me</a>
         </li>
         <li>
-          <a href="#work">
-            Portfolio
-          </a>
+          <a href="#work">Portfolio</a>
         </li>
         <li>
-          <a href="#contact">
-            Contact
-          </a>
+          <a href="#contact">Contact</a>
         </li>
         <li>
           <a href={PDF} target="_blank">
